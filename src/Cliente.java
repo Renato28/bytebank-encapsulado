47fe7509609406762024680dc79f6ac480242abd
Renato Nobrega <renatonobrega87@gmail.com>
2020-07-24 13:34:15 -0300

    Initial commit

---
+++ src/Cliente.java
@@ -0,0 +1,30 @@
+public class Cliente {
+
+    private String Cliente;
+    private String cpf;
+    private String profissao;
+
+    public String getCliente() {
+        return Cliente;
+    }
+
+    public void setCliente(String cliente) {
+        Cliente = cliente;
+    }
+
+    public String getCpf() {
+        return cpf;
+    }
+
+    public void setCpf(String cpf) {
+        this.cpf = cpf;
+    }
+
+    public String getProfissao() {
+        return profissao;
+    }
+
+    public void setProfissao(String profissao) {
+        this.profissao = profissao;
+    }
+}
